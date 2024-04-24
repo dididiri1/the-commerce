4c31453a92d13d30b25b68c3126a263d8447b73a
dididiri1 <dididiri1@naver.com>
2024-04-24 10:02:51 +0900

    first commit

---
+++ src/test/java/sample/thecommerce/TheCommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package sample.thecommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TheCommerceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
